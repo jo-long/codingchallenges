5b3b9bcefdca93220a4f0024b522598a9571c59d
jo-long <157486614+jo-long@users.noreply.github.com>
2024-01-29 11:53:42 -0500

    Update main.js

--- main.js
+++ main.js
@@ -1,30 +1,8 @@
-const canvas = document.getElementById("canvas");
-const ctx = canvas.getContext("2d");
+let cards = document.querySelectorAll(".card");
 
-let stars = [];
-let mouseX = 0;
-
-let speed = 1;
-
-for(let i = 0; i < 800; i++){
-    stars[i] = new Star(canvas.width, canvas.height);
-}
-
-canvas.addEventListener("mousemove", function(e){
-    mouseX = e.clientX - e.target.getBoundingClientRect().left;
+cards.forEach(function(e){
+    const link = e.querySelector(".hiddenLink").href
+    e.addEventListener("click", function(e){
+        window.location.href = link;
+    });
 });
-
-function draw(){
-    speed = (mouseX * 50) / canvas.width;
-    ctx.clearRect(0, 0, canvas.width, canvas.height);
-    ctx.fillStyle = "black";
-    ctx.fillRect(0, 0, canvas.width, canvas.height);
-    ctx.translate(canvas.width/2, canvas.height/2);
-    for(let i = 0; i < stars.length; i++){
-        stars[i].update(speed, canvas.width, canvas.height);
-        stars[i].show(ctx, canvas);
-    }
-    ctx.setTransform(1, 0, 0, 1, 0, 0);
-}
-
-setInterval(draw, 33.33);
