abcf2e205ffbcffe8b3af127765581d589f9d31a
jo-long <157486614+jo-long@users.noreply.github.com>
2024-01-30 01:17:47 -0500

    Delete Homepage directory

--- Homepage/main.js
+++
@@ -1,8 +0,0 @@
-let cards = document.querySelectorAll(".card");
-
-cards.forEach(function(e){
-    const link = e.querySelector(".hiddenLink").href
-    e.addEventListener("click", function(e){
-        window.location.href = link;
-    });
-});
