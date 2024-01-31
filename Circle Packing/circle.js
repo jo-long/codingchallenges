f5b7079b9d37902efa3b156c462c860efb306e1e
jo-long <157486614+jo-long@users.noreply.github.com>
2024-01-30 19:57:44 -0500

    Add files via upload

---
+++ Circle Packing/circle.js
@@ -0,0 +1,31 @@
+class Circle{
+    constructor(x, y, color){
+        this.x = x;
+        this.y = y;
+        this.color = color;
+        this.r = 2;
+        this.growing = true;
+    }
+
+    grow(){
+        if(this.growing){
+            this.r += 1;
+        }
+    }
+
+    show(ctx){
+        ctx.beginPath();
+        ctx.arc(this.x, this.y, this.r, 0, 2 * Math.PI);
+        ctx.fillStyle = this.color;
+        ctx.fill();
+    }
+
+    touchingEdge(width, height){
+        return (
+            this.x + this.r >= width ||
+            this.x - this.r <= 0 ||
+            this.y + this.r >= height ||
+            this.y - this.r <= 0
+        );
+    }
+}
---
+++ Circle Packing/main.js
@@ -0,0 +1,87 @@
+const canvas = document.getElementById("canvas");
+const ctx = canvas.getContext("2d");
+
+let circles = [];
+let imageData;
+let img = new Image();
+img.src = "cat.jpg";
+
+img.onload = () => {
+    ctx.drawImage(img, 0, 0);
+    imageData = ctx.getImageData(0, 0, canvas.width / 2, canvas.height);
+};
+
+function draw(){
+    let circlesToSpawn = 10;
+    let count = 0;
+    let attempts = 0;
+
+    while(count < circlesToSpawn){
+        let newCircle = createCircle();
+
+        if(newCircle !== null){
+            circles.push(newCircle);
+            count++
+        }
+
+        attempts++;
+
+        if(attempts > 1000){
+            //stop setInterval
+            break;
+        }
+    }
+
+    for(let i = 0; i < circles.length; i++){
+        let circle = circles[i];
+
+        if(circle.growing){
+            if(circle.touchingEdge()){
+                circle.growing = false;
+            }else{
+                for(let j = 0; j < circles.length; j++){
+                    let other = circles[j];
+                    if(circle !== other){
+                        let d = Math.sqrt(Math.pow(other.x - circle.x, 2) + Math.pow(other.y - circle.y, 2));
+                        let distance = circle.r + other.r;
+
+                        if(d - 1 < distance){
+                            circle.growing = false;
+                            break;
+                        }
+                    }
+                }
+            }
+        }
+
+        circle.show();
+        circle.grow();
+    }
+}
+
+function createCircle(){
+    let x = Math.random() * (img.width / 2);
+    let y = Math.random() * img.height;
+
+    let valid = true;
+
+    for(let i = 0; i < circles.length; i++){
+        let circle = circles[i];
+        let d = Math.sqrt(Math.pow(x - circle.x, 2) + Math.pow(y - circle.y, 2));
+        
+        if(d - 2 < circle.r){
+            valid = false;
+            break;
+        }
+
+        if(valid){
+            let index = (Math.floor(x) + Math.floor(y) * img.width) * 4;
+            let r = imageData.data[index];
+            let g = imageData.data[index + 1];
+            let b = imageData.data[index + 2];
+            return new Circle(x, y, `rgb(${r}, ${g}, ${b})`);
+        }else{
+            return null;
+        }
+    }
+}
